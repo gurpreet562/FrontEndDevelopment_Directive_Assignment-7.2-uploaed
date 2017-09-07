29865f300eb634bdba7b8beef18d084d0d59ea24
gurpreet562 <kgurpreet562@gmail.com>
2017-09-08 00:22:58 +0530

    Assignment 7.2 uploaded

---
+++ directive2.component.ts
@@ -0,0 +1,20 @@
+import { Component,OnInit,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ title = 'List Of Employees';
+employees:any[]=[
+  {name:'abc',age:30},
+  {name:'cde',age:34},
+  {name:'fgh',age:34},
+  {name:'bmn',age:34}
+];
+
+
+
+}
+
